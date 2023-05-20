31ad1663ef7f45739757e07c149a3be75415adf5
Iana <yklutkophilips@yandex.ru>
2023-05-20 09:51:19 +0400

    Добавьте файлы проекта.

---
+++ Lab12/Lab12.cpp
@@ -0,0 +1,33 @@
+﻿// Создайте и продемонстрируйте работу функции int* generateRandomArray(int n), 
+//возвращающей указатель на массив из n случайных чисел. Массив создается внутри функции как статическая переменная.
+
+#include <iostream>
+using namespace std;
+
+int* generateRandomArray(int n)
+{
+    static int* arr = new int[n]; //создаем статическую переменную - целочисленный динамический массив из n элементов
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) //заполняем массив случайными числами 
+        arr[i] = rand() % 1000;
+    return arr;
+    delete[] arr; //очищаем память
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cout << "Введите кол-во элементов в массиве: " << endl;
+    cin >> n;
+    //вызываем функцию, которая создает массив из n случайных чисел и возвращает указатель на массив
+    int* arr2 = generateRandomArray(n); 
+    for (int i = 0; i < n; i++) //выводим массив в консоль
+        cout << i << " элемент массива = " << arr2[i] << endl;
+    system("pause");
+    return 0;
+}
+
+
+
+
+
---
+++ Lab3/Lab3.cpp
@@ -0,0 +1,27 @@
+﻿// Создайте и продемонстрируйте работу функции void showArray(int array[], int size), выводящей на консоль элементы массива.
+//Вывод реализовать внутри функции через 2 указателя int* start, int* end: на текущий элемент и последний элемент массива.
+
+
+#include <iostream>
+using namespace std;
+
+void showArray(int array[], int size)// создаем функцию для вывода в консоль элементов массива
+{
+    int i;
+    int* start = &i; //создаем указатель на текущий элемент массива
+    int* end = &size; //создаем указатель на последний элемент массива
+    for (*start = 0; *start < *end; i++) //выводим в консоль элементы массива через цикл
+        cout << *start << " элемент массива = " << array[*start] << endl;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int array[10]; //создаем целочисленный массив
+    int size = end(array) - begin(array);
+    srand(time(NULL));
+    for (int i = 0; i < size; i++) //заполняем массив случайными числами и выводим его в консоль
+    array[i] = rand() % 1000;
+    showArray(array, size);//вызываем функцию для вывода в консоль элементов массива
+    system("pause");
+    return 0;
+}
